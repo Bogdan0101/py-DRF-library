30b43b5167e473f14c371c13718d279d3ce6078e
unknown <kononovb777@gmail.com>
2025-12-09 11:47:44 +0200

    create app library

--- py_DRF_library/settings.py
+++ py_DRF_library/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-740qkl052jjx#2!(r)&f)7$a7q8k5rjx#enka$78edi#jo&djs"
+SECRET_KEY = "django-740qkl052jjx#2!(r)&f)7$a7q8k5rjx#enka$78edi#jo&djs"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "debug_toolbar",
+    "library"
     "user",
 ]
 
--- user/models.py
+++ user/models.py
@@ -7,7 +7,12 @@ from django.contrib.auth.models import (AbstractUser,
 class UserManager(DjangoUserManager):
     use_in_migrations = True
 
-    def _create_user(self, email, password, first_name=None, last_name=None, **extra_fields):
+    def _create_user(self,
+                     email,
+                     password,
+                     first_name=None,
+                     last_name=None,
+                     **extra_fields):
         if not email:
             raise ValueError("The given email must be set")
         if not first_name:
@@ -15,24 +20,45 @@ class UserManager(DjangoUserManager):
         if not last_name:
             raise ValueError("The given last name must be set")
         email = self.normalize_email(email)
-        user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
+        user = self.model(email=email,
+                          first_name=first_name,
+                          last_name=last_name,
+                          **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, email=None, password=None, first_name=None, last_name=None, **extra_fields):
+    def create_user(self,
+                    email=None,
+                    password=None,
+                    first_name=None,
+                    last_name=None,
+                    **extra_fields):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
-        return self._create_user(email, password, first_name, last_name, **extra_fields)
+        return self._create_user(email,
+                                 password,
+                                 first_name,
+                                 last_name,
+                                 **extra_fields)
 
-    def create_superuser(self, email, password, first_name=None, last_name=None, **extra_fields):
+    def create_superuser(self,
+                         email,
+                         password,
+                         first_name=None,
+                         last_name=None,
+                         **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         if extra_fields.get("is_staff") is not True:
             raise ValueError("Superuser must have is_staff=True.")
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
-        return self._create_user(email, password, first_name, last_name, **extra_fields)
+        return self._create_user(email,
+                                 password,
+                                 first_name,
+                                 last_name,
+                                 **extra_fields)
 
 
 class User(AbstractUser):
