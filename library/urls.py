f4ab32d74385bcc8e9deb68e2924e32f184f1169
unknown <kononovb777@gmail.com>
2025-12-14 18:16:55 +0200

    create urls

--- library/urls.py
+++ library/urls.py
@@ -3,11 +3,13 @@ from rest_framework import routers
 from library.views import (
     BookViewSet,
     BorrowingViewSet,
+    PaymentViewSet,
 )
 
 router = routers.DefaultRouter()
-router.register("book", BookViewSet, basename="books")
-router.register("borrowing", BorrowingViewSet, basename="borrowings")
+router.register("books", BookViewSet, basename="books")
+router.register("borrowings", BorrowingViewSet, basename="borrowings")
+router.register("payments", PaymentViewSet, basename="payments")
 urlpatterns = [path("", include(router.urls))]
 
 app_name = "library"
